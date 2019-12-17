65c8b4589cc1788300fda8f3ffe65f2b11cc8b31
itxtools <47816114+itxtools@users.noreply.github.com>
2019-12-17 19:55:25 +0500

    Update proper.user.js

--- proper.user.js
+++ proper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         I'm a proper student!
-// @updateURL    https://github.com/itxtools/dark-etis-theme/raw/master/dark-etis.user.js
-// @downloadURL  https://github.com/itxtools/dark-etis-theme/raw/master/dark-etis.user.js
+// @updateURL    https://github.com/itxtools/dark-etis-theme/raw/master/proper.user.js
+// @downloadURL  https://github.com/itxtools/dark-etis-theme/raw/master/proper.user.js
 // @supportURL   https://github.com/itxtools/dark-etis-theme/issues
 // @version      0.1
 // @author       itxtools

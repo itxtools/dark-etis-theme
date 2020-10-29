722cdce5a31125456501e81ea693ccb6c4a63497
itxtools <47816114+itxtools@users.noreply.github.com>
2020-10-29 14:29:19 +0500

    Create zoomFixer.user.js

---
+++ zoomFixer.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Etis ZoomeFixer
+// @namespace    https://github.com/itxtools/dark-etis-theme
+// @updateURL    https://github.com/itxtools/dark-etis-theme/raw/master/zoomFixer.user.js
+// @downloadURL  https://github.com/itxtools/dark-etis-theme/raw/master/zoomFixer.user.js
+// @supportURL   https://github.com/itxtools/dark-etis-theme/issues
+// @version      1.0.0
+// @description  Can you even code
+// @author       je09
+// @licence      MIT
+// @match        https://student.psu.ru/pls/stu_cus_et/stu.signs*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function zoomFixer(item, index) {
+        var linkItem = item.getElementsByTagName("a")[0];
+        if (linkItem && linkItem.href.toString().includes("%20")) {
+            linkItem.href = linkItem.href.toString().split("%20")[0];
+        }
+    }
+
+    Array.prototype.slice.call(document.getElementsByClassName("aud")).forEach(zoomFixer);
+})();
